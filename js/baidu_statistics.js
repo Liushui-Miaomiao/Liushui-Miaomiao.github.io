ec156442adf55c4497503e3306a2d17fe08ad552
Liushui_Miaomiao <2812745143@qq.com>
2020-09-08 20:17:30 +0800

    2020.09.08 update 01
    updata reslib.html

---
+++ js/baidu_statistics.js
@@ -0,0 +1,8 @@
+// 百度统计
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?93cd8845b5600acc63884317efb8404c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
---
+++ js/console.js
@@ -0,0 +1,8 @@
+console.log("每个人都有一个怀才的种子，\n");
+console.log("每个人都想找到自己的闪光点，\n");
+console.log("我们无畏，\n");
+console.log("我们前行……\n");
+console.log("感谢怀才的你，找到这里……\n");
+console.log("我们是，5DPLAY Game Studio\n");
+console.log("诚邀有技术的你，加入我们！\n");
+console.log("\n技术问题请加群：");
